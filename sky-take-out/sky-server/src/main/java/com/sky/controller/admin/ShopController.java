cbc586952701b11a07dc54beebe0201b636f6ebb
vectorx <uxiahnan@outlook.com>
2024-04-05 13:24:18 +0800

    feat: 店铺管理
    1、Redis序列化器：redisTemplate.setKeySerializer
    2、Knife4J：groupName("用户端接口")

---
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,64 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author VectorX
+ * @version V1.0
+ * @description 店铺相关接口
+ * @date 2024-04-05 13:07:52
+ */
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController
+{
+    public static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 获取店铺的营业状态
+     *
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺的营业状态")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate
+                .opsForValue()
+                .get(KEY);
+        log.info("获取到店铺的营业状态为：{}", status == 1 ?
+                                   "营业中" :
+                                   "打烊中");
+        return Result.success(status);
+    }
+
+    /**
+     * 设置店铺的营业状态
+     *
+     * @param status
+     * @return
+     */
+    @PutMapping("/{status}")
+    @ApiOperation("设置店铺的营业状态")
+    public Result setStatus(
+            @PathVariable
+                    Integer status) {
+        log.info("设置店铺的营业状态为：{}", status == 1 ?
+                                  "营业中" :
+                                  "打烊中");
+        redisTemplate
+                .opsForValue()
+                .set(KEY, status);
+        return Result.success();
+    }
+}
---
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,46 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author VectorX
+ * @version V1.0
+ * @description 店铺相关接口
+ * @date 2024-04-05 13:07:52
+ */
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController
+{
+    public static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 获取店铺的营业状态
+     *
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺的营业状态")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate
+                .opsForValue()
+                .get(KEY);
+        log.info("获取到店铺的营业状态为：{}", status == 1 ?
+                                   "营业中" :
+                                   "打烊中");
+        return Result.success(status);
+    }
+}
